ddca24f5cd28790d3f007b3eb3de0c3da7678a4e
rupinderkaur30-gif <rupi063094@gmail.com>
2021-02-18 21:55:02 -0500

    commit tree 99f6a2476e4e7bdc7450908bbb82a0c763f8eecb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(student_name, grade)
+        if @roster[grade] 
+            @roster[grade] << student_name
+        
+        else
+            @roster[grade] = [student_name]
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        sort_hash = {}
+        @roster.sort.each  do |grade, students_name|
+            sort_hash[grade] = (students_name.sort)
+        end
+        sort_hash
+    end
+end
